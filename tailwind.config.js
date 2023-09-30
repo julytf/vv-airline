386efda5269cee39e4d24dfa1f2c06c9146be66d
julyyvvvvv <julytf.liyc@gmail.com>
2023-09-30 09:51:38 +0700

    add: view template

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Views/**/*.{cshtml,css}', 
+    './Areas/**/*.{cshtml,css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
